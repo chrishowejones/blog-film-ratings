2bac0e7a65370efeb586cb62d13b5e1b84daafaa
Chris Howe-Jones <chris.howejones@gmail.com>
2018-12-22 08:12:49 +0000

    Duct app generated w/ +site +ataraxy +sqlite +example

---
+++ src/film_ratings/main.clj
@@ -0,0 +1,12 @@
+(ns film-ratings.main
+  (:gen-class)
+  (:require [clojure.java.io :as io]
+            [duct.core :as duct]))
+
+(duct/load-hierarchy)
+
+(defn -main [& args]
+  (let [keys (or (duct/parse-keys args) [:duct/daemon])]
+    (-> (duct/read-config (io/resource "film_ratings/config.edn"))
+        (duct/prep keys)
+        (duct/exec keys))))
